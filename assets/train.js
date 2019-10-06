47a64b25fbbeff2d64f5851dfd352a536858238e
Melissa Chiang <mchiang62@gmail.com>
2019-10-06 19:35:23 -0400

    firebase added

--- assets/train.js
+++ assets/train.js
@@ -1,4 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyD_H9bFN10DoS4at7gDnbB1FV53S1IFCPI",
+        authDomain: "train-scheduler-496fe.firebaseapp.com",
+        databaseURL: "https://train-scheduler-496fe.firebaseio.com",
+        projectId: "train-scheduler-496fe",
+        storageBucket: "train-scheduler-496fe.appspot.com",
+        messagingSenderId: "975963282752",
+        appId: "1:975963282752:web:ab2ed3272a60b0c8fa6ea5",
+        measurementId: "G-GD7G5CML2X"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+
+
+    $("#addNewTrain").on("click", function (event) {
+        event.preventDefault();
+
+        train = $("#train-name").val().trim();
+        dest = $("#destination").val().trim();
+        firstTrain = $("#first-train").val().trim();
+        freq = $("#frequency").val().trim();
+
+
+
+    });
 
 
 
@@ -23,14 +50,4 @@ $(document).ready(function(){
 
 
 
-
-
-    
 });
-
-
-
-
-
-
-
