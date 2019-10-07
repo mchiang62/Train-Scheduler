8fc5ffafeb1dec2a4a68dd16ea7d292972a1f126
Melissa Chiang <mchiang62@gmail.com>
2019-10-06 20:41:00 -0400

    adding moment

--- assets/train.js
+++ assets/train.js
@@ -44,9 +44,43 @@ $(document).ready(function () {
 
 
 
+    database.ref().on("child_added", function(childSnapshot) {
 
 
 
+    //   // Assumptions
+      var tFrequency = 3;
+
+    //   // Time is 3:30 AM
+      var firstTime = "03:30";
+  
+    //   // First Time (pushed back 1 year to make sure it comes before current time)
+      var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    //   console.log(firstTimeConverted);
+  
+  
+    //   // Difference between the times
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    //   console.log("DIFFERENCE IN TIME: " + diffTime);
+  
+    //   // Time apart (remainder)
+      var tRemainder = diffTime % tFrequency;
+    //   console.log(tRemainder);
+  
+      // Minute Until Train
+      var minUntilNextTrain = tFrequency - tRemainder;
+      console.log("MINUTES TILL TRAIN: " + minUntilNextTrain);
+    //   $("#min-row").append(tMinutesTillTrain);
+  
+      // Next Train
+      var nextTrain = moment().add(minUntilNextTrain, "minutes");
+      console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    //   $("#arrival-row").append(moment(nextTrain).format("hh:mm"));
+
+
+
+    });
+    
 
 
 
